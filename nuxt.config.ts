a36bed87a8271d28ed74679fe52c34bb527aa320
zhou-tao <zhoutao_233@163.com>
2023-09-19 11:06:35 +0800

    chore: add unocss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules: [
+    '@unocss/nuxt'
+  ],
+  devtools: {
+    enabled: true
+  },
+  typescript: {
+    shim: false
+  }
 })
---
+++ uno.config.ts
@@ -0,0 +1,92 @@
+import { FileSystemIconLoader } from '@iconify/utils/lib/loader/node-loaders'
+import { defineConfig, presetUno, presetAttributify, presetIcons, transformerVariantGroup, transformerDirectives } from 'unocss'
+
+export default defineConfig({
+  theme: {
+    colors: {
+      root_light: '#f5f6fa', // 最底层背景
+      root_dark: '#272727',
+      page_light: '#ffffff', // 基础布局背景
+      page_dark: '#18181B',
+      primary: 'var(--el-color-primary)',
+      main: 'var(--el-text-color-primary)',
+      regular: 'var(--el-text-color-regular)',
+      secondary: 'var(--el-text-color-secondary)',
+      placeholder: 'var(--el-text-color-placeholder)',
+      light: 'var(--el-color-info-light-9)',
+      light_hover: 'var(--el-color-info-light-8)'
+    },
+    height: {
+      header: '64px',
+      footer: '50px',
+      tab: 'var(--tab-view-height)',
+      content: 'var(--content-base-height)'
+    },
+    minHeight: {
+      content: 'var(--content-base-height)'
+    }
+  },
+  shortcuts: {
+    'bg-root': 'bg-root_light dark:bg-root_dark',
+    'bg-page': 'bg-page_light dark:bg-page_dark',
+    'transition-base': 'transition-all duration-150 ease-in-out',
+    'page-base': 'min-h-content overflow-x-hidden box-border',
+    'page-card': 'page-base bg-page rounded px-6 py-5',
+    'page-pure': 'page-base bg-page rounded h-content',
+    'flex-center': 'flex items-center justify-center'
+  },
+  rules: [
+    [
+      'text-brand-gradient',
+      {
+        'color': 'transparent',
+        'background-image': 'linear-gradient(to right, #00ff8f, #00a1ff)',
+        'background-clip': 'text'
+      }
+    ],
+    [
+      'bg-gradient-light',
+      {
+        'background-image':
+          'linear-gradient( 135deg, #ABDCFF30 20%, #0396FF20 100%)'
+      }
+    ],
+    [
+      'bg-gradient-dark',
+      {
+        'background-image':
+          'linear-gradient( 135deg, #2A5470 20%, #4C4177 100%)'
+      }
+    ],
+    [
+      'shadow-card-dark',
+      {
+        'box-shadow': '0 0 12px 0 rgb(0 0 0 / 9%)'
+      }
+    ]
+  ],
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      warn: true,
+      collections: {
+        app: FileSystemIconLoader('./src/assets/icons')
+      },
+      extraProperties: {
+        'display': 'inline-block',
+        'vertical-align': 'middle',
+        'margin': '0 4px'
+      }
+    })
+  ],
+  transformers: [
+    transformerVariantGroup(),
+    transformerDirectives()
+  ],
+  safelist: [
+    'i-ep-menu',
+    'i-ep-avatar',
+    'i-ep-info-filled'
+  ]
+})
