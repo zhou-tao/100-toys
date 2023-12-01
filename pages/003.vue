d2f75af2858a5bee16bbb8156daa1da2ae4a9988
zhou-tao <zhoutao_233@163.com>
2023-12-01 17:52:10 +0800

    feat(003): change music cover

--- pages/003.vue
+++ pages/003.vue
@@ -41,12 +41,12 @@
 
 <template>
   <audio ref="audio" loop src="/mp3/The Climb - Miley Cyrus.mp3" />
-  <div class="container" w="600px <sm:320px" h="600px <sm:320px" relative bg-black border="solid 1 gray">
+  <div class="container" w="600px <sm:320px" h="600px" relative bg-white dark:bg-black border="solid 1 gray">
     <canvas ref="canvas" />
     <div class="player animate-spin" :style="{ 'animation-play-state': playing ? 'running' : 'paused' }" w120px h120px absolute z2 rounded-full border="24 #27272A solid" op60 cursor-pointer @click="toggleAudio">
-      <img src="~/assets/images/mp3_poster.png" alt="The Climb" width="120" rounded-full>
+      <img src="~/assets/images/poster.png" alt="The Climb" height="120" rounded-full ml-35px>
     </div>
-    <div class="icon" :style="{ opacity: playing ? 0 : 0.7 }" i-ri-play-circle-line absolute z3 w60px h60px text="#71717A" pointer-events-none transition-all ease-in-out duration-300 />
+    <div class="icon" :style="{ opacity: playing ? 0 : 0.7 }" i-ri-play-circle-line absolute z3 w60px h60px text="#71717A60" pointer-events-none transition-all ease-in-out duration-300 />
   </div>
 </template>
 
@@ -57,13 +57,13 @@ canvas {
 }
 
 .player {
-  top: calc(50% - 84px);
+  top: calc(24% - 84px);
   left: calc(50% - 84px);
   animation-duration: 8s;
 }
 
 .icon {
-  top: calc(50% - 30px);
+  top: calc(24% - 30px);
   left: calc(50% - 30px);
 }
 </style>
